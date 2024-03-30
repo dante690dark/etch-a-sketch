e4cf9f2d70a5c2f6b85cb84c0afbb75e7426943c
Alexander Manns <dante690dark@gmail.com>
2024-03-29 23:12:40 -0500

    add button to square's size

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
   
-    const size = 16
-    const gridSize = size * size
-    const container = document.querySelector('.container')
+    const button = document.querySelector('button')
+  
+    button.addEventListener('click', () => {
 
-    for (let i = 0; i < gridSize; i++) {
+      const container = document.querySelector('.container')
+      container.innerHTML = ''
+
+      let size 
+
+      do {
+        size = prompt('Enter grid size (max 100):')
+      }while (size > 100)
+
+      const gridSize = size * size
+        
+      for (let i = 0; i < gridSize; i++) {
+        
+        const gridSquare = document.createElement('div')
+
+        gridSquare.style.cssText = `width: calc(100% / ${size}); 
+                                    height: calc(100% / ${size});  
+                                    border: 1px solid #ddd; 
+                                    box-sizing: border-box;`
+
+        gridSquare.addEventListener('mouseover', () => {
+          gridSquare.style.backgroundColor = 'black'
+        })
       
-      const gridSquare = document.createElement('div')
-
-      gridSquare.style.cssText = `width: calc(100% / ${size}); 
-                                  height: calc(100% / ${size});  
-                                  border: 1px solid #ddd; 
-                                  box-sizing: border-box;`
-
-      gridSquare.addEventListener('mouseover', function() {
-        gridSquare.style.backgroundColor = 'black'
-      })
-    
-      container.appendChild(gridSquare)
-    }
+        container.appendChild(gridSquare)
+      }   
+    })
   }
 );
