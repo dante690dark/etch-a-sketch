d8c29135b7b1636735a2b6c6883f7e99817a56ab
Alexander Manns <dante690dark@gmail.com>
2024-03-29 21:49:50 -0500

    add hover effect in each square

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
   
-    const prueba = 16
-    const gridSize = prueba * prueba
+    const size = 16
+    const gridSize = size * size
+    const container = document.querySelector('.container')
 
     for (let i = 0; i < gridSize; i++) {
       
       const gridSquare = document.createElement('div')
 
-      gridSquare.style.cssText = `width: calc(100% / ${prueba}); 
-                                  height: calc(100% / ${prueba});  
+      gridSquare.style.cssText = `width: calc(100% / ${size}); 
+                                  height: calc(100% / ${size});  
                                   border: 1px solid #ddd; 
                                   box-sizing: border-box;`
+
+      gridSquare.addEventListener('mouseover', function() {
+        gridSquare.style.backgroundColor = 'black'
+      })
     
-      document.querySelector('.container').appendChild(gridSquare)
+      container.appendChild(gridSquare)
     }
   }
 );
