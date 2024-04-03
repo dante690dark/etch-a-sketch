e818185dbd30f5afcc8d306dd7aa28158594e9c8
Alexander Manns <dante690dark@gmail.com>
2024-04-02 21:33:58 -0500

    add default value

--- script.js
+++ script.js
@@ -1,42 +1,51 @@
+const INITIAL_VALUE = 16
 document.addEventListener('DOMContentLoaded', () => {
   
-    const button = document.querySelector('button')
-  
-    button.addEventListener('click', () => {
+  let size = INITIAL_VALUE
+  const button = document.querySelector('button')
 
-      const container = document.querySelector('.container-grid')
-      container.innerHTML = ''
+  const etchSketch = (str=false) =>{
+    const container = document.querySelector('.container-grid')
+    container.innerHTML = ''
 
-      let size 
-
+    if(str){ 
       do {
         size = prompt('Enter grid size (max 100):')
-      }while (size > 100)
+      }while (size > 100) 
+    }
 
-      const gridSize = size ** 2
-        
-      for (let i = 0; i < gridSize; i++) {
-        
-        const gridSquare = document.createElement('div')
+    size ??= INITIAL_VALUE
 
-        gridSquare.style.cssText = `width: calc(100% / ${size}); 
-                                    height: calc(100% / ${size});  
-                                    border: 1px solid #ddd; 
-                                    box-sizing: border-box;`
-
-        gridSquare.addEventListener('mouseover', () => {
-
-          if(!gridSquare.style.backgroundColor){
-            gridSquare.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`
-          }
-
-          if(gridSquare.style.backgroundColor){
-            gridSquare.style.opacity = +gridSquare.style.opacity + 0.1
-          }
-        })
+    const gridSize = size ** 2
       
-        container.appendChild(gridSquare)
-      }   
+    for (let i = 0; i < gridSize; i++) {
+      
+      const gridSquare = document.createElement('div')
+
+      gridSquare.style.cssText = `width: calc(100% / ${size}); 
+                                  height: calc(100% / ${size});  
+                                  border: 1px solid #ddd; 
+                                  box-sizing: border-box;`
+
+      gridSquare.addEventListener('mouseover', () => {
+
+        if(!gridSquare.style.backgroundColor){
+          gridSquare.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`
+        }
+
+        if(gridSquare.style.backgroundColor){
+          gridSquare.style.opacity = +gridSquare.style.opacity + 0.1
+        }
+      })
+    
+      container.appendChild(gridSquare)
+    }   
+  }
+
+  etchSketch()
+  
+  button.addEventListener('click', () => {
+      etchSketch(true)
     })
   }
 )
